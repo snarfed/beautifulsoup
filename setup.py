d1afcbd857916be0238c37b4e01a7bcfbb58c486
Leonard Richardson <leonard.richardson@canonical.com>
2012-02-02 08:43:52 -0500

    Added docs.

--- setup.py
+++ setup.py
@@ -6,11 +6,14 @@ except ImportError:
     # 2.x
     from distutils.command.build_py import build_py
 
-
-setup(name="BeautifulSoup",
-    version="4.0b2",
+setup(name="BeautifulSoup4",
+    version="4.0b3",
     author="Leonard Richardson",
     url="http://www.crummy.com/software/BeautifulSoup/",
     packages=['bs4', 'bs4.builder', 'bs4.tests'],
-    cmdclass = {'build_py':build_py}
+    cmdclass = {'build_py':build_py},
+    classifiers=[
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+    ],
 )
