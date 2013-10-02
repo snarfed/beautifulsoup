80125c4efca7f4e020158f8a8a741c36d56cad02
Leonard Richardson <leonardr@segfault.org>
2013-10-02 08:33:46 -0400

    Prep for release.

--- bs4/__init__.py
+++ bs4/__init__.py
@@ -17,7 +17,7 @@ http://www.crummy.com/software/BeautifulSoup/bs4/doc/
 """
 
 __author__ = "Leonard Richardson (leonardr@segfault.org)"
-__version__ = "4.3.1"
+__version__ = "4.3.2"
 __copyright__ = "Copyright (c) 2004-2013 Leonard Richardson"
 __license__ = "MIT"
 
